f290591abb55974b4ceb5f25af9c7b310ae45483
David Bau <david.bau@gmail.com>
2013-12-24 11:10:30 +0000

    Use bowercopy for test dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,21 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    bowercopy: {
+      options: {
+        clean: true
+      },
+      test: {
+        options: {
+          destPrefix: "test/lib"
+        },
+        files: {
+          "qunit.js" : "qunit/qunit/qunit.js",
+          "qunit.css" : "qunit/qunit/qunit.css",
+          "require.js" : "requirejs/require.js"
+        }
+      }
+    },
     uglify: {
       all: {
         files: {
@@ -30,6 +45,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-bowercopy');
 
   grunt.registerTask("default", ["uglify", "qunit"]);
 };
