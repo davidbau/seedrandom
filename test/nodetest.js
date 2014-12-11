d638c901818272a57bfcf1fdbb83d4dd1edb59f8
David Bau <david.bau@gmail.com>
2014-12-11 08:38:41 -0500

    Add coverage tests for state and AMD.

--- test/nodetest.js
+++ test/nodetest.js
@@ -1,13 +1,17 @@
 var assert = require("assert");
 var seedrandom = require("../seedrandom");
+var requirejs = require("requirejs");
+requirejs.config({
+  baseUrl: __dirname
+});
 
 describe("Nodejs API Test", function() {
 
-var original = Math.random,
-    result, r, xprng, obj, as2, as3, autoseed1, myrng,
-    firstprng, secondprng, thirdprng;
-
 it('should pass basic tests.', function() {
+  var original = Math.random,
+      result, r, xprng, obj, as2, as3, autoseed1, myrng,
+      firstprng, secondprng, thirdprng;
+
   result = Math.seedrandom('hello.');
   firstprng = Math.random;
   assert(original !== firstprng, "Should change Math.random.");
@@ -31,7 +35,7 @@ it('should pass basic tests.', function() {
   assert(r != 0.7316977468919549, "Should not be 'hello.'#3");
   autoseed1 = r;
 
-  it('should be able to add entropy.');
+  // should be able to add entropy.
   result = Math.seedrandom('added entropy.', { entropy:true });
   assert.equal(result.length, 256, "Should return short seed.");
   thirdprng = Math.random;
@@ -160,6 +164,52 @@ it('should pass basic tests.', function() {
   rng = seedrandom('hello.', {});
   assert.equal(typeof(rng), 'function', "Should return a function.");
   r = rng();
+  assert.equal(r, 0.9282578795792454, "Should be 'hello.'#1");
+  assert(original === Math.random, "Should not change Math.random.");
+  assert(original !== rng, "PRNG should not be Math.random.");
+});
+
+it('should support state api.', function() {
+  // Verify that there is no state method
+  var dummy = seedrandom('hello');
+  var unexpected = -1;
+  var expected = -1;
+  try {
+    unexpected = dummy.state();
+  } catch(e) {
+    expected = 1;
+  }
+  assert.equal(unexpected, -1);
+  assert.equal(expected, 1);
+  var count = 0;
+  for (var x in dummy) {
+    count += 1;
+  }
+  assert.equal(count, 0);
+
+  // Verify that a state method can be added
+  var saveable = seedrandom("secret-seed", {state: true});
+  var ordinary = seedrandom("secret-seed");
+  for (var j = 0; j < 1e2; ++j) {
+    assert.equal(ordinary(), saveable());
+  }
+  var virgin = seedrandom("secret-seed");
+  var saved = saveable.state();
+  var replica = seedrandom("", {state: saved});
+  for (var j = 0; j < 1e2; ++j) {
+    var r = replica();
+    assert.equal(r, saveable());
+    assert.equal(r, ordinary());
+    assert(r != virgin());
+  }
+});
+
+it('should support requirejs in node.', function() {
+  var original = Math.random;
+  var rsr = requirejs('../seedrandom');
+  var rng = rsr('hello.');
+  assert.equal(typeof(rng), 'function', "Should return a function.");
+  var r = rng();
   assert.equal(r, 0.9282578795792454, "Should be 'hello.'#1");
   assert(original === Math.random, "Should not change Math.random.");
   assert(original !== rng, "PRNG should not be Math.random.");
