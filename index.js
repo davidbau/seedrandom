519f500037f11a646a50b7a614120a7b53d86f10
David Bau <david.bau@gmail.com>
2015-01-12 02:51:46 +0000

    Fix tyche-i.

--- index.js
+++ index.js
@@ -27,12 +27,12 @@
 // Math.random on Chrome.
 //
 //            speed  period       BigCrush failures
-// xor128     1.5x   2^128-1      MatrixRank, LinearComp
-// xsadd      1.6x   2^128-1      several when bits are reversed
-// xorwow     1.7x   2^192-2^32   CollisionOver, SimpPoker, LinearComp
-// xorshift7  1.8x   2^256-1      none
-// xor4096    1.9x   2^4128-2^32  none
-// tychei     2.0x   2^128-1      none (dervied from ChaCha)
+// xor128     1.7x   2^128-1      MatrixRank, LinearComp
+// xsadd      1.9x   2^128-1      several when bits are reversed
+// xorwow     1.9x   2^192-2^32   CollisionOver, SimpPoker, LinearComp
+// xorshift7  2.1x   2^256-1      none
+// xor4096    2.1x   2^4128-2^32  none
+// tychei     2.2x   2^128-1      none
 //
 // These speedy algorithsm all return 32 bits of randomness.
 //
@@ -59,7 +59,7 @@
 // are included here, and benchmarked against calls to
 // Javascript's native Math.random() on Chrome on a fast PC:
 //
-// native: 17 nanoseconds per call, 1.0x native random.
+// native: 15.5 nanoseconds per call, 1.0x native random.
 //
 // xor128 is a very fast pure xor-shift generator from Marsaglia's
 // original paper.  Though xor128 passes the Diehard tests, it fails
@@ -67,13 +67,13 @@
 // proposed xorwow, which adds a simple Weyl generator
 // to add more robustness.
 
-// xor128: 25 nanoseconds per call, 1.5x native random.
+// xor128: 25.7 nanoseconds per call, 1.7x native random.
 // A pure xor-shift generator by George Marsaglia.
 // Period: 2^128-1.
 // Reported to fail: MatrixRank and LinearComp.
 var xor128 = require('./xor/xor128');
 
-// xorwow: 28.5 nanoseconds per call, 1.7x native random.
+// xorwow: 29.6 nanoseconds per call, 1.9x native random.
 // George Marsaglia's 160-bit xor-shift combined plus weyl.
 // Period: 2^192-2^32
 // Reported to fail: CollisionOver, SimpPoker, and LinearComp.
@@ -88,7 +88,7 @@ var xorwow = require('./xor/xorwow');
 // weaknesses in the lower bits; unfortunatly, that improved
 // algorithm requires 64-bit arithmatic and is not fast in Javascript.
 
-// xsadd: 28 nanoseconds per call, 1.6x native random.
+// xsadd: 28.9 nanoseconds per call, 1.9x native random.
 // Mutsuo Saito and Makoto Matsumoto's xorshift with an addition.
 // Period: 2^128-1.
 // Fails no tests in normal mode.
@@ -100,7 +100,7 @@ var xsadd = require('./xor/xsadd');
 // than Marsaglia's original three.  It is a 7-shift generator
 // with 256 bits, that passes BigCrush with no systmatic failures.
 
-// xorshift7: 31 nanoseconds per call, 1.8x native random.
+// xorshift7: 32.4 nanoseconds per call, 2.1x native random.
 // François Panneton & Pierre L'ecuyer's 7-shift generator with 256 bits.
 // Period 2^256-1.
 // No systematic BigCrush failures reported.
@@ -112,17 +112,21 @@ var xorshift7 = require('./xor/xorshift7');
 // be useful if you have many generators and need to avoid
 // collisions.
 
-// xor4096: 31.5 nanoseconds per call, 1.9x native random.
+// xor4096: 32.9 nanoseconds per call, 2.1x native random.
 // Richard Brent's 4096-bit "xorgens" xor shift plus weyl.
 // Period: 2^4128-2^32.
 // No systematic BigCrush failures reported.
 var xor4096 = require('./xor/xor4096');
 
-// tychei: 34 nanoseconds per call, 2.0x native random.
+// Tyche-i, by Samuel Neves and Filipe Araujo, is a bit-shifting random
+// number generator derived from ChaCha, a modern stream cipher.
+// https://eden.dei.uc.pt/~sneves/pubs/2011-snfa2.pdf
+
+// tychei: 34.5 nanoseconds per call, 2.2x native random.
 // Richard Brent's 4096-bit "xorgens" xor shift plus weyl.
 // Period: 2^4128-2^32.
 // No systematic BigCrush failures reported.
-var xor4096 = require('./xor/xor4096');
+var tychei = require('./xor/tychei');
 
 var sr = require('./seedrandom');
 sr.xor128 = xor128;
@@ -130,5 +134,6 @@ sr.xsadd = xsadd;
 sr.xorwow = xorwow;
 sr.xorshift7 = xorshift7;
 sr.xor4096 = xor4096;
+sr.tychei = tychei;
 
 module.exports = sr;
