61f8b4272b2d6d60fc91b07e61548c0e56a13ec9
David Bau <david.bau@gmail.com>
2017-02-12 00:40:51 -0500

    Ignore crypto under browserify instead of stubbing.

--- seedrandom.js
+++ seedrandom.js
@@ -193,10 +193,14 @@ function mixkey(seed, key) {
 //
 function autoseed() {
   try {
-    if (nodecrypto) { return tostring(nodecrypto.randomBytes(width)); }
-    var out = new Uint8Array(width);
-    (global.crypto || global.msCrypto).getRandomValues(out);
-
+    var out;
+    if (nodecrypto && (out = nodecrypto.randomBytes)) {
+      // The use of 'out' to remember randomBytes makes tight minified code.
+      out = out(width);
+    } else {
+      out = new Uint8Array(width);
+      (global.crypto || global.msCrypto).getRandomValues(out);
+    }
     return tostring(out);
   } catch (e) {
     var browser = global.navigator,
--- test/browserified.js
+++ test/browserified.js
@@ -1,21 +1,8 @@
 require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (global){
-// mimic a subset of node's crypto API for the browser
 
-function randomBytes(width) {
-  var out = new Uint8Array(width);
-  (global.crypto || global.msCrypto).getRandomValues(out);
-  return out;
-}
-
-module.exports = {
-  randomBytes: randomBytes
-}
-
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}],2:[function(require,module,exports){
-
-},{}],3:[function(require,module,exports){
+arguments[4][1][0].apply(exports,arguments)
+},{"dup":1}],3:[function(require,module,exports){
 /*
 Copyright 2014 David Bau.
 
@@ -211,10 +198,14 @@ function mixkey(seed, key) {
 //
 function autoseed() {
   try {
-    if (nodecrypto) { return tostring(nodecrypto.randomBytes(width)); }
-    var out = new Uint8Array(width);
-    (global.crypto || global.msCrypto).getRandomValues(out);
-
+    var out;
+    if (nodecrypto && (out = nodecrypto.randomBytes)) {
+      // The use of 'out' to remember randomBytes makes tight minified code.
+      out = out(width);
+    } else {
+      out = new Uint8Array(width);
+      (global.crypto || global.msCrypto).getRandomValues(out);
+    }
     return tostring(out);
   } catch (e) {
     var browser = global.navigator,
