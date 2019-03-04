460cc1e4a3fecdf69b6455de39eb53e462790fd1
David Bau <david.bau@gmail.com>
2019-03-04 09:49:55 -0500

    Update year.

--- seedrandom.js
+++ seedrandom.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2014 David Bau.
+Copyright 2019 David Bau.
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
