ffe3128a11e55e8c0460778c33404abe1e4d339c
8times12 <8times12@gmail.com>
2020-08-04 21:51:02 +0900

    modify seedrandom declaration

--- index.d.ts
+++ index.d.ts
@@ -1,46 +1,65 @@
-export interface SeedrandomPrng {
+/**
+ * Generate a seeded random number.
+ * 
+ * @param seed
+ * @param options
+ * @param callback
+ */
+export interface Seedrandom {
+  (seed?: any, options?: boolean): Prng;
+
+  <TReturn>(
+    seed: any,
+    options: Options<Callback<TReturn>>
+  ): TReturn | Prng | string;
+
+  <TReturn>(
+    seed: any,
+    options: Options<undefined>,
+    callback: Callback<TReturn>
+  ): TReturn;
+}
+
+export interface Prng {
   (): number;
-  int32: () => number;
-  quick: () => number;
-  double: () => number;
+  int32(): number;
+  quick(): number;
+  double(): number;
+  state?(): Arc4;
 }
 
-export interface SeedrandomArc4State {
-  i?: number;
-  j?: number;
-  S?: number[];
+export interface MathOptions {
+  entropy?: boolean;
+  state?: Arc4State;
 }
 
-class SeedrandomArc4 implements SeedrandomArc4State {
+export interface Options<TPass> extends MathOptions {
+  global?: boolean;
+  pass?: TPass;
+}
+
+export interface Callback<TReturn> {
+  (prng: Prng, seed: string, is_math_call: true, state: object): TReturn;
+}
+
+export interface Arc4State {
+  i: number;
+  j: number;
+  S: number[];
+}
+
+export class Arc4 implements Arc4State {
   constructor(key: number[]);
   g(count: number): number;
 }
 
-export type SeedrandomCallback<R> = (prng: SeedrandomPrng, seed: string, is_math_call: boolean, state: SeedrandomArc4State) => R;
-
-export interface SeedrandomMathOptions {
-  entropy?: boolean;
-}
-export interface SeedrandomOverrideOptions extends SeedrandomMathOptions {
-  global: true;
-  state?: SeedrandomArc4State;
-}
-export interface SeedrandomPassOptions extends SeedrandomMathOptions {
-  pass: SeedrandomCallback
-}
-export interface SeedrandomOptions extends SeedrandomMathOptions {
-  global?: boolean;
-  pass?: SeedrandomCallback;
-  state?: SeedrandomArc4State;
-}
-
-export function seedrandom(seed?: any): SeedrandomArc4;
-export function seedrandom(seed: any, options: SeedrandomOverrideOptions): string;
-export function seedrandom<R>(seed: any, options: SeedrandomPassOptions<R>): R;
-export function seedrandom(seed?: any, options?: SeedrandomOptions, callback: SeedrandomCallback<R>): SeedrandomArc4;
+/**
+ * The seedrandom function. See {@type Seedrandom} for details.
+ */
+export const seedrandom: Seedrandom;
 
 export declare global {
   interface Math {
-    seedrandom: (seed?: any, options?: SeedrandomMathOptions) => string;
+    seedrandom: (seed?: any, options?: MathOptions) => string;
   }
 }
